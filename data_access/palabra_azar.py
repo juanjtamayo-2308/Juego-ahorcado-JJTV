7e8b65de905d1e40a871dcd8244ebfc11cea5225
juanjtamayo-2308 <juanj.tamayo01@gmail.com>
2026-06-29 10:52:17 -0500

    Update palabra_azar.py

--- data_access/palabra_azar.py
+++ data_access/palabra_azar.py
@@ -2,4 +2,9 @@
 #Juan Tamayo
 
 import random
-from data_access.bnco_palabras import 
+from data_access.bnco_palabras import obtener_list_words
+
+def seleccionar_palabra_azar():
+  palabras = obtener_list_words()
+  return random.choice(palabras)
+  
