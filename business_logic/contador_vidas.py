5f02cf73380ee85e60495f35fbcf35a75e6c9236
juanjtamayo-2308 <juanj.tamayo01@gmail.com>
2026-06-29 11:35:11 -0500

    Update contador_vidas.py

--- business_logic/contador_vidas.py
+++ business_logic/contador_vidas.py
@@ -2,12 +2,14 @@
 #Juan Tamayo
 
 class GestorIntentos:
-  def init(self,limite_vidas=5):
+  def __init__(self,limite_vidas=5):
       self.intentos_restantes = limite_vidas
-    def resta_intento(self):
+    
+  def resta_intento(self):
       if self.intentos_restantes > 0:
         self.intentos_restantes -= 1
-        return self.intentos_restantes
-      def vidas(self):
-        return self.intentos_restantes > 0
+      return self.intentos_restantes
+        
+  def vidas(self):
+      return self.intentos_restantes > 0
         
